f3d2cdf4d3e089f1c4266250a392ca72e11c040f
Steffen Müller <arxisos@gmail.com>
2024-02-29 16:38:44 +0100

    runtests: add missing tests

--- src/SpectralFactorMOR/test/runtests.jl
+++ src/SpectralFactorMOR/test/runtests.jl
@@ -5,5 +5,7 @@ Pkg.add(url="https://github.com/Algopaul/PortHamiltonianBenchmarkSystems.jl/")
 @testset "spectralFactorMORdescriptor" begin
     include("./test_irka.jl")
     include("./test_lyapunov.jl")
+    include("./test_prbt.jl")
     include("./test_riccati.jl")
+    include("./test_sfmor.jl")
 end
