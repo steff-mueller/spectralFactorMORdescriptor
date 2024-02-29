d183f8d3056e1346de10575ec045afdea7df7695
Steffen Müller <arxisos@gmail.com>
2024-02-29 13:30:33 +0100

    Add tests for `sfmor`

---
+++ src/SpectralFactorMOR/test/test_sfmor.jl
@@ -0,0 +1,38 @@
+module SfmorTests
+
+using Test
+using Random
+using PortHamiltonianBenchmarkSystems
+using LinearAlgebra
+using SparseArrays
+using SpectralFactorMOR
+using DescriptorSystems
+
+include("./problems.jl")
+using .TestProblems
+
+const problems = [
+    ("irka index 0 SISO", test_setup_DAE0_RCL(1), 1e-3),
+    ("irka index 0 MIMO", test_setup_DAE0_RCL(2), 1e-1),
+    ("irka index 1 SISO", test_setup_DAE1_RCL(1), 1e-3),
+    ("irka index 1 MIMO", test_setup_DAE1_RCL(2), 1e-1),
+    ("irka index 2 SISO", test_setup_DAE2_RCL(1), 1e-3),
+    ("irka index 2 MIMO", test_setup_DAE2_RCL(2), 1e-1)
+]
+
+for problem in problems
+    testname, sys, tol = problem
+    @testset "$testname" begin
+        Random.seed!(0)
+        sysinf, syssp, = splitsys(sys)
+        rom, result = sfmor(sys, 10, IRKAOptions())
+        rominf, romsp, = splitsys(rom)
+        @test rom isa AbstractDescriptorStateSpace
+        @test result isa IRKAResult
+        @test all(dss2rm(todss(rominf)) .≈ dss2rm(todss(sysinf)))
+        @test h2normsp(syssp - romsp) < tol
+        @test ispr(rom)[1]
+    end
+end
+
+end # module SfmorTests
